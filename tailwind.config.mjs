72c4f62ea7eeb2008799d209c113a61a9629d226
NEOCHOCAPIC <juan.santibanezc@gmail.com>
2024-10-27 19:28:14 -0300

    Se agrega footer y demas partes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'custom-yellow': 'rgb(250, 184, 24)', 
+					gris: '#a0a0a0'// Agrega tu color aquí
+			  },
+			backgroundImage: {
+				'custom-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0), #213944)',
+			  }
+		},
 	},
 	plugins: [],
 }
